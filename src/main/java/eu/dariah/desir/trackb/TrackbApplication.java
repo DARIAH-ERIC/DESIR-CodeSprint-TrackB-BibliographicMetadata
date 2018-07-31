5104798f349ed3f7028d74e31ea0e87e16c4da44
yoann <yoann.moranville@gmail.com>
2018-07-31 17:47:37 +0200

    maven skeleton for the track B with spring boot

---
+++ src/main/java/eu/dariah/desir/trackb/TrackbApplication.java
@@ -0,0 +1,12 @@
+package eu.dariah.desir.trackb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackbApplication.class, args);
+	}
+}
