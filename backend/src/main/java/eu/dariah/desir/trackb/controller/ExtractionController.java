164762f5594e85191d7628e3b0051c26c5c80a99
Robert Jäschke <jaeschke@l3s.de>
2018-08-01 16:43:14 +0200

    cleaned up controller

--- backend/src/main/java/eu/dariah/desir/trackb/controller/ExtractionController.java
+++ backend/src/main/java/eu/dariah/desir/trackb/controller/ExtractionController.java
@@ -32,123 +32,139 @@ import eu.dariah.desir.trackb.service.MetadataExtractor;
 @Controller
 public class ExtractionController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ExtractionController.class);
-    private static final String ERROR_JSON = "{\"error\": true}";
+	private static final Logger LOG = LoggerFactory.getLogger(ExtractionController.class);
+	private static final String ERROR_JSON = "{\"error\": true}";
 
-    private final MetadataExtractor me;
-    private final BibSonomyAdaptor adaptor;
+	private final MetadataExtractor me;
+	private final BibSonomyAdaptor adaptor;
 
-    @Autowired
-    public ExtractionController(MetadataExtractor me, BibSonomyAdaptor adaptor) {
-        this.me = me;
-        this.adaptor = adaptor;
-    }
+	@Autowired
+	public ExtractionController(MetadataExtractor me, BibSonomyAdaptor adaptor) {
+		this.me = me;
+		this.adaptor = adaptor;
+	}
 
-    /**
-     * @param file
-     * @return
-     */
-    @PostMapping(value="/store")
-    public @ResponseBody String storeInBibSonomy(@RequestParam(value = "file", required = false) MultipartFile file) {
-        List<YetAnotherBibliographicItem> items;
+	/**
+	 * @param file
+	 * @return
+	 */
+	@PostMapping(value="/store")
+	public @ResponseBody String storeInBibSonomy(@RequestParam(value = "file", required = false) MultipartFile file) {
+		List<YetAnotherBibliographicItem> items;
 
-        try {
-            if (file != null) {
-                String fileName = file.getName();
-                File json_file;
-                try {
-                    json_file = new File(System.getProperty("java.io.tmpdir") + "/" + file.getName());
-                    file.transferTo(json_file);
-                    LOG.info("Successfully uploaded " + fileName + " into " + json_file.getName());
-                } catch (Exception e) {
-                    LOG.error("Failed to upload " + fileName, e);
-                    return ERROR_JSON;
-                }
-                try {
-                    items = me.extractItems(json_file);
-                    adaptor.storeItems(items);
-                } catch(Exception e) {
-                	LOG.error("Failed to extract items", e);
-                    return ERROR_JSON;
-                }
-            } else {
-            	LOG.error("Received empty file");
-                return ERROR_JSON;
-            }
-        } catch (InvalidParameterException ipe) {
-            LOG.error("Error with parameters: ", ipe);
-            return ERROR_JSON;
-        }
+		try {
+			if (file != null) {
+				String fileName = file.getName();
+				File jsonFile;
+				try {
+					jsonFile = new File(System.getProperty("java.io.tmpdir") + "/" + file.getName());
+					file.transferTo(jsonFile);
+					LOG.info("Successfully uploaded " + fileName + " into " + jsonFile.getName());
+				} catch (Exception e) {
+					LOG.error("Failed to upload " + fileName, e);
+					return ERROR_JSON;
+				}
+				try {
+					items = me.extractItems(jsonFile);
+					adaptor.storeItems(items);
+				} catch(Exception e) {
+					LOG.error("Failed to extract items", e);
+					return ERROR_JSON;
+				}
+			} else {
+				LOG.error("Received empty file");
+				return ERROR_JSON;
+			}
+		} catch (InvalidParameterException ipe) {
+			LOG.error("Error with parameters: ", ipe);
+			return ERROR_JSON;
+		}
 
-        return null;
-    }
+		return null;
+	}
 
 
-    /**
-     * @param file
-     * @param text
-     * @return
-     */
-    @PostMapping(value="/extract", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public @ResponseBody String handleFileUpload(
-            @RequestParam(value = "file", required = false) MultipartFile file,
-            @RequestParam(value = "text", required = false) String text) {
+	/**
+	 * @param file
+	 * @param text
+	 * @return
+	 */
+	/**
+	 * @param file
+	 * @param text
+	 * @return
+	 */
+	@PostMapping(value="/extract", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public @ResponseBody String handleFileUpload(
+			@RequestParam(value = "file", required = false) MultipartFile file,
+			@RequestParam(value = "text", required = false) String text) {
 
-        List<YetAnotherBibliographicItem> items;
+		final List<YetAnotherBibliographicItem> items;
 
-        try {
-            if (file == null && text == null) {
-                throw new InvalidParameterException("The request does neither contain a file nor a text string. We need one or the other.");
-            } else if (file != null && text != null) {
-                throw new InvalidParameterException("The request does contain both a file and a text string. We only need one of them.");
-            }
-            if (file != null) {
-                String fileName = file.getName();
-                File jsonFile;
-                try {
-                    jsonFile = new File(System.getProperty("java.io.tmpdir") + "/" + file.getName());
-                    file.transferTo(jsonFile);
-                    LOG.info("Successfully uploaded " + fileName + " into " + jsonFile.getName());
-                } catch (Exception e) {
-                    LOG.error("Failed to upload " + fileName + " => " + e.getMessage());
-                    return ERROR_JSON;
-                }
-                try {
-                    items = me.extractItems(jsonFile);
-                } catch(Exception e) {
-                    return ERROR_JSON;
-                }
-            } else {
-                try {
-                    items = me.extractItems(text);
-                } catch(Exception e) {
-                    return ERROR_JSON;
-                }
-            }
-        } catch (InvalidParameterException ipe) {
-            LOG.error("Error with parameters: ", ipe);
-            return ERROR_JSON;
-        }
+		try {
+			// sanity checks for parameters
+			if (file == null && text == null) {
+				throw new InvalidParameterException("The request does neither contain a file nor a text string. We need one or the other.");
+			} else if (file != null && text != null) {
+				throw new InvalidParameterException("The request does contain both a file and a text string. We only need one of them.");
+			}
+			
+			// handle file or text
+			if (file != null) {
+				final String fileName = file.getName();
+				final File jsonFile;
+				try {
+					jsonFile = new File(System.getProperty("java.io.tmpdir") + "/" + file.getName());
+					file.transferTo(jsonFile);
+					LOG.info("Successfully uploaded " + fileName + " into " + jsonFile.getName());
+				} catch (Exception e) {
+					LOG.error("Failed to upload " + fileName + " => " + e.getMessage());
+					return ERROR_JSON;
+				}
+				try {
+					items = me.extractItems(jsonFile);
+				} catch(Exception e) {
+					return ERROR_JSON;
+				}
+			} else {
+				try {
+					items = me.extractItems(text);
+				} catch(Exception e) {
+					return ERROR_JSON;
+				}
+			}
 
-        if(items != null) {
-            // converting bib items to json
-            StringBuilder sb = new StringBuilder();
-            for (YetAnotherBibliographicItem item : items) {
-                final ObjectMapper mapper = new ObjectMapper();
-                mapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
-                try {
-                    final String result = mapper
-                            .writerWithView(JsonViews.Public.class)
-                            .writeValueAsString(item);
-                    sb.append(result);
-                } catch (JsonProcessingException e) {
-                    LOG.error(e.toString());
-                }
-            }
-            return sb.toString();
-        }
+			if (items == null) {
+				LOG.error("Could not extract any items");
+				return ERROR_JSON;
+			}
+			return toJson(items);
 
-        return null;
-    }
+		} catch (InvalidParameterException ipe) {
+			LOG.error("Error with parameters: ", ipe);
+			return ERROR_JSON;
+		}
+	}
+
+	/**
+	 * Convert bibliographic items to JSON
+	 * 
+	 * @param items
+	 * @return
+	 */
+	private static String toJson(List<YetAnotherBibliographicItem> items) {
+		final StringBuilder sb = new StringBuilder();
+
+		final ObjectMapper mapper = new ObjectMapper();
+		mapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
+		try {
+			sb.append(mapper.writerWithView(JsonViews.Public.class).writeValueAsString(items));
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			LOG.error("TODO", e);
+		}
+
+		return sb.toString();
+	}
 }
 
