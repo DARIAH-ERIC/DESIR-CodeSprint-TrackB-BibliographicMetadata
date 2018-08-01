799dc4a6d53b4a993967653c4a0f9a7daffe4d87
yoann <yoann.moranville@gmail.com>
2018-08-01 13:50:37 +0200

    Set limit to file upload to 50MB

--- src/main/java/eu/dariah/desir/trackb/conf/FormConfiguration.java
+++ src/main/java/eu/dariah/desir/trackb/conf/FormConfiguration.java
@@ -1,10 +1,20 @@
 package eu.dariah.desir.trackb.conf;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class FormConfiguration {
+/**
+ * This class is to modify the configuration of the Form handler in Spring
+ */
+@Configuration
+public class FormConfiguration implements WebMvcConfigurer {
+    /**
+     * Setting the size of uploaded file to about 50MB
+     * @return The MultiPartResolver knowing the size limit of the upload
+     */
     @Bean
     public MultipartResolver multipartResolver() {
         CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
