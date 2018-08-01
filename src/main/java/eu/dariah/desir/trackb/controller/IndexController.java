60aba612ea5f3ffef7ab2fb185ace7e3ee887a5a
yoann <yoann.moranville@gmail.com>
2018-08-01 10:01:20 +0200

    Add logging with slf4j

--- src/main/java/eu/dariah/desir/trackb/controller/IndexController.java
+++ src/main/java/eu/dariah/desir/trackb/controller/IndexController.java
@@ -3,8 +3,17 @@ package eu.dariah.desir.trackb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * The main controller which serves the Index and Error pages
+ */
 @Controller
 public class IndexController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(IndexController.class);
 
     /**
      * Index page controller of the application
@@ -12,6 +21,7 @@ public class IndexController {
      */
     @GetMapping(value = {"/", "/index"})
     public String index() {
+        LOG.info("Reached the index page");
         return "index";
     }
 
