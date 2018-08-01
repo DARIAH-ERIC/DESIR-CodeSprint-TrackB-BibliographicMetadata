d25301e29dd2163321ccf67663369dbfd05f92b3
Christoph Hube <hube@l3s.de>
2018-08-01 12:31:56 +0200

    added bib_list extraction to ExtractionController

--- src/main/java/eu/dariah/desir/trackb/controller/ExtractionController.java
+++ src/main/java/eu/dariah/desir/trackb/controller/ExtractionController.java
@@ -1,5 +1,7 @@
 package eu.dariah.desir.trackb.controller;
 
+import eu.dariah.desir.trackb.model.YetAnotherBibliographicItem;
+import eu.dariah.desir.trackb.service.GrobidModelConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -11,6 +13,7 @@ import eu.dariah.desir.trackb.service.MetadataExtractor;
 
 import java.io.File;
 import java.security.InvalidParameterException;
+import java.util.List;
 
 /**
  * Created by hube on 8/1/2018.
@@ -32,6 +35,12 @@ public class ExtractionController {
     public @ResponseBody String handleFileUpload(
             @RequestParam(value = "file", required = false) MultipartFile file,
             @RequestParam(value = "text", required = false) String text) {
+
+        GrobidModelConverter converter = new GrobidModelConverter();
+        MetadataExtractor me = new MetadataExtractor(converter);
+        me.init();
+        List<YetAnotherBibliographicItem> bib_list;
+
         try {
             if (file == null && text == null) {
                 throw new InvalidParameterException("The request does not contain a file nor a text string. We need one or " +
@@ -46,15 +55,25 @@ public class ExtractionController {
                     File json_file = new File(System.getProperty("java.io.tmpdir") + "/" + file.getName());
                     file.transferTo(json_file);
                     LOG.info("Successfully uploaded " + file_name + " into " + json_file.getName());
+
+                    //extract bibliographical data
+                    bib_list = me.extractItems(json_file);
+
                 } catch (Exception e) {
                     LOG.error("Failed to upload " + file_name + " => " + e.getMessage());
                 }
             } else if (text != null) {
-                    //handle string
+                //handle string
+
+                //extract bibliographical data
+                bib_list = me.extractItems(text);
             }
         } catch (InvalidParameterException ipe) {
             LOG.error("Error with parameters: ", ipe);
         }
+
+        //TODO: convert bib_list to json and return json
+
         return "{}";
     }
 }
