f6f1a7442455d8821c76cc5d009502ee0a707565
Robert Jäschke <jaeschke@l3s.de>
2018-08-01 17:52:37 +0200

    fixed bug

--- backend/src/test/java/eu/dariah/desir/trackb/controller/ExtractionControllerTest.java
+++ backend/src/test/java/eu/dariah/desir/trackb/controller/ExtractionControllerTest.java
@@ -49,7 +49,7 @@ public class ExtractionControllerTest {
         MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 
-        final byte[] testData = IOUtils.toByteArray(this.getClass().getClassLoader().getResourceAsStream("EAD-ODD_ A_solution_for_project-oriented_EAD_schemes.pdf"));
+        final byte[] testData = IOUtils.toByteArray(this.getClass().getClassLoader().getResourceAsStream("EAD-ODD_A_solution_for_project-oriented_EAD_schemes.pdf"));
         pdfFile = new MockMultipartFile("file", "EAD-ODD_A_solution_for_project-oriented_EAD_schemes.pdf", MediaType.APPLICATION_PDF_VALUE,
                 testData);
     }
