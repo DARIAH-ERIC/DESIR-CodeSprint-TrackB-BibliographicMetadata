4a2bd84addf51a250c76132ebe581167f61e786e
Christoph Hoffmann <christoph.hoffmann@oeaw.ac.at>
2018-08-02 11:29:38 +0200

    build modified for post

--- frontend/src/store/modules/entries/index.js
+++ frontend/src/store/modules/entries/index.js
@@ -3,11 +3,22 @@
 /* eslint-disable no-underscore-dangle */
 const state = {
   entries: [],
+  valid: [],
+  required: ['entryType', 'title', 'year', 'authors', 'editors']
 };
 
 const getters = {
   getEntry: (state) => (id) => {
     return state.entries[id];
+  },
+  isValid: (state) => (id) => {
+    let res = [];
+    for (var i = 0; i < state.required.length; i++) {
+      console.log(state.entries[id][state.required[i]]);
+      if(!state.entries[id][state.required[i]]) res.push(state.required[i]);
+      if(state.entries[id].tags.length < 1) res.push('tags');
+    }
+    return res;
   },
 };
 
