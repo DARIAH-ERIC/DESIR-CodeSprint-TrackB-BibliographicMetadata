05c31df14c79efd78e5fcc7cdcfa19e40b2c9a8a
yoann <yoann.moranville@gmail.com>
2018-08-02 11:22:46 +0200

    Use helper for converting stroing json to list of object

---
+++ backend/src/main/java/eu/dariah/desir/trackb/helper/JsonHelper.java
@@ -0,0 +1,20 @@
+package eu.dariah.desir.trackb.helper;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import eu.dariah.desir.trackb.model.YetAnotherBibliographicItem;
+
+import java.util.List;
+
+public class JsonHelper {
+    public static List<YetAnotherBibliographicItem> convert(String json) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        List<YetAnotherBibliographicItem> yetAnotherBibliographicItems = null;
+        try {
+            yetAnotherBibliographicItems = mapper.readValue(json, new TypeReference<List<YetAnotherBibliographicItem>>() {});
+        } catch (Exception e) {
+            throw new Exception("Error converting JSON collection to List<YetAnotherBibliographicItem>");
+        }
+        return yetAnotherBibliographicItems;
+    }
+}
