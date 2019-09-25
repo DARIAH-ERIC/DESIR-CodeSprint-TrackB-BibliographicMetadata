85afcbec1abc03c853f8193c38ee0c1e3c4d8041
Sergej Wildemann <wildemann@l3s.de>
2019-09-25 10:16:58 +0200

    frondend: add delete entry

--- frontend/src/components/entries.vue
+++ frontend/src/components/entries.vue
@@ -8,17 +8,25 @@
           <td>{{ props.item.entryType }}</td>
           <td>{{ props.item.year }}</td>
           <td>
-            <v-btn
-              @click="openEditDialog($store.state.entries.entries[props.item.idx])"
-              color="info"
-            >Edit</v-btn>
-          </td>
-          <td>
             <v-alert :value="isValid(props.item.idx).length==0" type="success">Item ready to submit.</v-alert>
             <v-alert
               :value="isValid(props.item.idx).length>0"
               type="warning"
             >Item missing {{ isValid(props.item.idx) }}.</v-alert>
+          </td>
+          <td>
+            <v-btn
+              text
+              icon
+              @click="openEditDialog($store.state.entries.entries[props.item.idx])"
+            >
+              <v-icon>edit</v-icon>
+            </v-btn>
+          </td>
+          <td>
+            <v-btn text icon @click="itemdelete(props.item.idx)" color="red">
+              <v-icon>delete</v-icon>
+            </v-btn>
           </td>
         </template>
       </v-data-table>
@@ -46,8 +54,8 @@ export default {
         { text: "booktitle", value: "booktitle" },
         { text: "entryType", value: "entryType" },
         { text: "year", value: "year" },
-        { text: "edit", value: "" },
-        { text: "status", value: "" }
+        { text: "status", value: "" },
+        { text: "edit", value: "" }
       ]
     };
   },
@@ -59,8 +67,12 @@ export default {
       for (var i = 0; i < this.entries.length; i++) {
         o.push(this.entries[i]);
       }
-      console.log(o);
+      console.log("store items: " + o);
       axios.post("/store", o);
+    },
+    itemDelete(idx) {
+      console.log("delete item: " + idx);
+      this.entries.splice(idx, 1);
     }
   },
   computed: {
